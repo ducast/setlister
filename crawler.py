dafb0798d4e1a89db1a0bcd296983b24b1db9be0
Eduardo Castanho <eduardo-castanho@hotmail.com>
2017-09-11 10:00:03 -0300

    multiple pages

--- crawler.py
+++ crawler.py
@@ -2,42 +2,46 @@
 import wget
 import os
 
-band = "red hot chili peppers"
+band = "the offspring"
 # band = input("Type the name of the band: ")
-band = band.replace(' ','+')
-url = 'http://www.setlist.fm/search?query='+band
-band_filename = wget.download(url,out='band.html')
-band_file = open(band_filename)
-band_html = band_file.read()
-artists = band_html.split('<h3>Artist</h3>')
-artist = artists[1].split('href="')
-link = artist[1].split('" rel')
-url = 'http://www.setlist.fm/'+link[0]
-url = url.replace('&amp;','&')
-setlists_filename = wget.download(url,out='setlists.html')
-setlists_file = open(setlists_filename)
-setlists_html = setlists_file.read()
-setlists_split = setlists_html.split('href="setlist/')[1:11]
+pages = 3
 songs_dict = {}
-for setlist in setlists_split:
-    link = setlist.split('" title')
-    url = 'http://www.setlist.fm/setlist/'+link[0]
-    setlist_filename = wget.download(url,out='setlist.html')
-    setlist_file = open(setlist_filename)
-    setlist_html = setlist_file.read()
-    if not "Sorry, there are no songs in this setlist yet" in setlist_html:
-        songs = setlist_html.split('songLabel')[1:]
-        for song in songs:
-            label = song.split('>')
-            label = label[1].split('<')
-            label = label[0].replace('&amp;','&').replace('&#039;',"'")
-            print('\n%s\n'%label)
-            if label in songs_dict:
-                songs_dict[label] += 1
-            else:
-                songs_dict[label] = 1
-
 songs_list = []
+
+band = band.replace(' ','+')
+
+for page in range(pages):
+    url = 'http://www.setlist.fm/search?page=%d&query=%s'%(page+1,band)
+    band_filename = wget.download(url,out='band.html')
+    band_file = open(band_filename)
+    band_html = band_file.read()
+    artists = band_html.split('<h3>Artist</h3>')
+    artist = artists[1].split('href="')
+    link = artist[1].split('" rel')
+    url = 'http://www.setlist.fm/'+link[0]
+    url = url.replace('&amp;','&')
+    setlists_filename = wget.download(url,out='setlists.html')
+    setlists_file = open(setlists_filename)
+    setlists_html = setlists_file.read()
+    setlists_split = setlists_html.split('href="setlist/')[1:11]
+    for setlist in setlists_split:
+        link = setlist.split('" title')
+        url = 'http://www.setlist.fm/setlist/'+link[0]
+        setlist_filename = wget.download(url,out='setlist.html')
+        setlist_file = open(setlist_filename)
+        setlist_html = setlist_file.read()
+        if not "Sorry, there are no songs in this setlist yet" in setlist_html:
+            songs = setlist_html.split('songLabel')[1:]
+            for song in songs:
+                label = song.split('>')
+                label = label[1].split('<')
+                label = label[0].replace('&amp;','&').replace('&#039;',"'")
+                print('\n%s\n'%label)
+                if label in songs_dict:
+                    songs_dict[label] += 1
+                else:
+                    songs_dict[label] = 1
+
 for key in songs_dict:
     if songs_dict[key] < 10:
         songs_list.append('00' + str(songs_dict[key]) + ' ' + key + '\n')
@@ -56,6 +60,6 @@ for f in files:
         except:
             pass
 
-f=open(band.replace('+','_')+'_setlister.txt','w')
+f=open('results/'+band.replace('+','_')+'_setlister.txt','w')
 f.writelines(songs_list)
 f.close()
